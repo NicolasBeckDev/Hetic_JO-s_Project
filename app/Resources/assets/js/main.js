580a1110c603adc18b21a4f7efe7f6e49c9073b1
Quentin Massias <quentin.massias28630@gmail.com>
2018-01-30 22:02:36 +0100

    add image for connection and registration page

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -3,6 +3,7 @@ const $ = require('jquery');
 require('bootstrap-sass');
 
 require('../img/background-homepage.jpg');
+require('../img/connexion.png');
 
 require('./layout/header')();
 require('./pages/form/form-widget')();
