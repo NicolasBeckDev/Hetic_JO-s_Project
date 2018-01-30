bdc1b8025facee08cf31404f52f87ce4bb867fde
Quentin Massias <quentin.massias28630@gmail.com>
2018-01-30 22:31:07 +0100

    revert change

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -4,6 +4,7 @@ require('bootstrap-sass');
 
 require('../img/background-homepage.jpg');
 require('../img/connexion.png');
+require('../img/inscription.png');
 
 require('./layout/header')();
 require('./pages/form/form-widget')();
